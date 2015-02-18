d2bda38798e354dc2a53297e740e8f990abdad2e
Hassan Hayat <hassan.hayat7@gmail.com>
2015-02-18 18:03:17 +0000

    Updated Example
    Updated Example to match new API changes

--- src/Example.elm
+++ src/Example.elm
@@ -1,13 +1,32 @@
-import Check (..)
-import Random (..)
-
 import Text (plainText)
+import Random (..)
+import Check (..)
+import Signal (..)
+import List
+
+prop_numberIsIdentity : Property
+prop_numberIsIdentity = propertyN 100 "number is identity" (\n -> n == n) (int 0 100)
+
+prop_numberIsOdd : Property
+prop_numberIsOdd = property "number is odd" (\n -> n % 2 == 1) (int 0 100)
+
+prop_reverseReverseList : Property
+prop_reverseReverseList =
+  property "reverse reverse list" (\l -> List.reverse (List.reverse l) == l) (list 100 (int 0 100))
+
+prop_discontinuous : Property
+prop_discontinuous =
+  property "discontinuous" (\x -> (x - 1) // (x - 1) == 1) (int 0 1000)
+
+test : Signal TestOutput
+test =
+  continuousCheck
+    [ prop_numberIsIdentity
+    , prop_discontinuous
+    , prop_numberIsOdd
+    , prop_reverseReverseList
+    ]
 
 
-tests =
-  check
-  [ property "No Identity" (\x -> x /= x) (float 0 2)
-  , property2 "Bad Addition Subtraction Inverse" (\a b -> (a - b - 1) == (a + (-b))) (float 0 100) (float 0 100)  
-  ]
-
-main = plainText tests
+main =
+  display <~ test
