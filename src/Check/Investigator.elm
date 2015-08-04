7b49c4e9d3fdec2f76c4afbca31aa2e65db60524
Hassan Hayat <hassan.hayat7@gmail.com>
2015-08-04 04:47:34 +0100

    Added the functional hits!

--- src/Check/Investigator.elm
+++ src/Check/Investigator.elm
@@ -12,9 +12,12 @@ migrating from local to cloud-based.
 @docs Investigator, investigator
 
 # Basic Investigator Generators
-@docs void, bool, order, int, float, percentage, char, ascii, unicode, string, maybe, result, list, array, tuple, tuple3, tuple4, tuple5, func, func2, func3, func4, func5
+docs void, bool, order, int, float, percentage, char, ascii, unicode, string, maybe, result, list, array, tuple, tuple3, tuple4, tuple5, func, func2, func3, func4, func5
 
 -}
+import RoseTree exposing (RoseTree(..))
+import Lazy exposing (Lazy, lazy, force)
+import Lazy.List exposing (LazyList, (:::), (+++))
 import Array  exposing (Array)
 import Shrink exposing (Shrinker)
 import Random exposing (Generator)
@@ -29,22 +32,259 @@ import Random.Result
 import Random.List
 import Random.Array
 
-{-| An Investigator type is a Random Generator paired with a shrinking strategy,
-or Shrinker. Shrinkers are defined in `elm-shrink`.
+flattenGenerator : Generator (Generator a) -> Generator a
+flattenGenerator genOfGens =
+  Random.customGenerator <|
+    \seed ->
+        let
+            (gen, seed2) = Random.generate genOfGens seed
+            (value, seed3) = Random.generate gen seed2
+        in
+            (value, seed3)
+
+{-| An Investigator is a Random Generator that generates values along with
+their corresponding shrink trees.
 -}
-type alias Investigator a =
-  { generator : Generator a
-  , shrinker  : Shrinker a
-  }
+type alias Investigator a = Generator (RoseTree a)
+
+{-| Create a shrink tree from a shrinker and a value.
+-}
+shrinkTree : Shrinker a -> a -> RoseTree a
+shrinkTree shrink a =
+  Rose a (Lazy.List.map (shrinkTree shrink) (shrink a))
 
 {-| Investigator constructor. Construct an Investigator from a generator and
 a shrinker.
 -}
 investigator : Generator a -> Shrinker a -> Investigator a
-investigator generator shrinker =
-  { generator = generator
-  , shrinker  = shrinker
-  }
+investigator generator shrink =
+  Random.map (shrinkTree shrink) generator
+
+
+{-| Turn a tree of generators into a generator of trees.
+-}
+unwind : RoseTree (Generator a) -> Generator (RoseTree a)
+unwind treeOfGenerators =
+  Random.customGenerator <|
+    \seed ->
+      let
+          (values, seeds) = unzipTree <|
+            RoseTree.map (\gen -> Random.generate gen seed) treeOfGenerators
+
+          seed2 =
+            RoseTree.root seeds
+
+      in
+          (values, seed2)
+
+{-}
+unwindList : List (RoseTree a) -> RoseTree (List a)
+unwindList trees =
+  let
+      -- List a
+      roots =
+        List.map RoseTree.root trees
+
+      -- List (LazyList (RoseTree a))
+      children =
+-}
+
+unwindLazyList : LazyList (RoseTree a) -> RoseTree (LazyList a)
+unwindLazyList trees =
+  let
+      -- LazyList a
+      roots =
+        Lazy.List.map RoseTree.root trees
+
+      -- LazyList (LazyList (RoseTree a))
+      children =
+        Lazy.List.map RoseTree.children trees
+
+      -- LazyList (RoseTree (LazyList a))
+      unwoundChildren =
+        Lazy.List.map unwindLazyList children
+
+  in
+      Rose roots unwoundChildren
+
+unwindList : List (RoseTree a) -> RoseTree (List a)
+unwindList trees =
+  trees
+  |> Lazy.List.fromList
+  |> unwindLazyList
+  |> RoseTree.map Lazy.List.toList
+
+
+unwindArray : Array (RoseTree a) -> RoseTree (Array a)
+unwindArray trees =
+  trees
+  |> Lazy.List.fromArray
+  |> unwindLazyList
+  |> RoseTree.map Lazy.List.toArray
+
+
+{-| Create an investigator from a constant value that does not shrink.
+-}
+constant : a -> Investigator a
+constant =
+  RoseTree.singleton >> Random.constant
+
+{-| Create an investigator from a random generator without shrinking.
+-}
+fromGenerator : Generator a -> Investigator a
+fromGenerator generator =
+  Random.map RoseTree.singleton generator
+
+{-| Apply a custom shrinker to an investigator.
+-}
+applyShrinker : Shrinker a -> Investigator a -> Investigator a
+applyShrinker shrink investigator =
+  Random.map (RoseTree.root >> shrinkTree shrink) investigator
+
+--applyShrinkerConstructor : (Shrinker a -> Shrinker b) -> Investigator a -> Investigator b
+--applyShrinkerConstructor constructor investigator =
+
+getChildrenRoots : RoseTree a -> LazyList a
+getChildrenRoots =
+  RoseTree.children >> Lazy.List.map RoseTree.root
+
+
+flatMap : (a -> Investigator b) -> Investigator a -> Investigator b
+flatMap f investigator =
+  Random.customGenerator <|
+    \seed ->
+      let
+          -- treeOfAs : RoseTree a
+          (treeOfAs, seed2) =
+            Random.generate investigator seed
+
+          -- generatorOfTrees : Generator (RoseTree b)
+          generatorOfTrees =
+            treeOfAs
+            |> RoseTree.map f
+            |> unwind
+            |> Random.map RoseTree.flatten
+
+      in
+          Random.generate generatorOfTrees seed2
+
+andThen : Investigator a -> (a -> Investigator b) -> Investigator b
+andThen =
+  flip flatMap
+
+
+unzipTree : RoseTree (a, b) -> (RoseTree a, RoseTree b)
+unzipTree (Rose (x, y) children) =
+  let
+      (xs, ys) = unzipList <|
+        Lazy.List.map unzipTree children
+  in
+      (Rose x xs, Rose y ys)
+
+unzipList : LazyList (a, b) -> (LazyList a, LazyList b)
+unzipList list =
+  let
+      step (x,y) (xs, ys) =
+        (x ::: xs, y ::: ys)
+  in
+      Lazy.List.reduce step (Lazy.List.empty, Lazy.List.empty) list
+
+
+{-| Map a function over an investigator.
+-}
+map : (a -> b) -> Investigator a -> Investigator b
+map =
+  RoseTree.map >> Random.map
+
+{-|-}
+map2 : (a -> b -> c) -> Investigator a -> Investigator b -> Investigator c
+map2 =
+  RoseTree.map2 >> Random.map2
+
+{-| Chain mapping operations.
+-}
+andMap : Investigator (a -> b) -> Investigator a -> Investigator b
+andMap =
+  map2 (<|)
+
+{-|-}
+map3 : (a -> b -> c -> d) -> Investigator a -> Investigator b -> Investigator c -> Investigator d
+map3 f l1 l2 l3 =
+  f
+    `map` l1
+    `andMap` l2
+    `andMap` l3
+
+{-|-}
+map4 : (a -> b -> c -> d -> e) -> Investigator a -> Investigator b -> Investigator c -> Investigator d -> Investigator e
+map4 f l1 l2 l3 l4 =
+  f
+    `map` l1
+    `andMap` l2
+    `andMap` l3
+    `andMap` l4
+
+{-|-}
+map5 : (a -> b -> c -> d -> e -> f) -> Investigator a -> Investigator b -> Investigator c -> Investigator d -> Investigator e -> Investigator f
+map5 f l1 l2 l3 l4 l5 =
+  f
+    `map`    l1
+    `andMap` l2
+    `andMap` l3
+    `andMap` l4
+    `andMap` l5
+
+{-|-}
+zip : Investigator a -> Investigator b -> Investigator (a, b)
+zip =
+  map2 (,)
+
+{-|-}
+zip3 : Investigator a -> Investigator b -> Investigator c -> Investigator (a, b, c)
+zip3 =
+  map3 (,,)
+
+{-|-}
+zip4 : Investigator a -> Investigator b -> Investigator c -> Investigator d -> Investigator (a, b, c, d)
+zip4 =
+  map4 (,,,)
+
+{-|-}
+zip5 : Investigator a -> Investigator b -> Investigator c -> Investigator d -> Investigator e -> Investigator (a, b, c, d, e)
+zip5 =
+  map5 (,,,,)
+
+{-| Re-export of Random.Extra.merge.
+Choose between two investigators with a 50-50 chance.
+Useful for merging two generators that cover different areas of the same type.
+-}
+merge : Investigator a -> Investigator a -> Investigator a
+merge =
+  Random.merge
+
+{-| Re-export of Random.Extra.frequency
+Create a investigator that chooses an investigator from a list of
+likelihood-investigator pairs based on the provided likelihood.
+The likelihood of a given investigator being chosen is its likelihood divided
+by the sum of all likelihoods. A default investigator must be provided
+in the case that the list is empty or that the sum of the likelihoods is 0.
+The default investigator is not selected otherwise.
+Note that the absolute values of the likelihoods is always taken.
+-}
+frequency : List (Float, Investigator a) -> Investigator a -> Investigator a
+frequency =
+  Random.frequency
+
+
+{-| Similar to `frequency` where all investigators are equally likely to be
+selected. Note that, as is the case with `frequency`, in the case that the
+provided list is non-empty, the default investigator will not be selected.
+-}
+oneOf : List (Investigator a) -> Investigator a -> Investigator a
+oneOf list default =
+  frequency
+    (List.map (\gen -> (1, gen)) list)
+    default
 
 
 {-| Investigator void. Uses a constant generator and the `void` shrinker from
@@ -82,6 +322,11 @@ int =
   in
       investigator generator Shrink.int
 
+{-|
+-}
+positiveInt : Investigator Int
+positiveInt =
+  rangeInt 1 (Random.maxInt)
 
 {-| Investigator int constructor. Generates random ints between a given `min`
 and a given `max` value.
@@ -173,26 +418,46 @@ string =
     (Shrink.string)
 
 
-{-| Investigator maybe constructor. Generates random maybe values from a given
-investigator generator using the `maybe` generator constructor from
-elm-random-extra and the `maybe` shrinker constructor from elm-shrink.
+{-| Investigator maybe constructor.
+Generates random maybe values from a given investigator generator.
 -}
 maybe : Investigator a -> Investigator (Maybe a)
-maybe inv =
-  investigator
-    (Random.Maybe.maybe inv.generator)
-    (Shrink.maybe inv.shrinker)
+maybe investigator =
+  merge (map Just investigator) (constant Nothing)
+
 
 {-| Investigator result constructor. Generates random result values from a given
 investigator generator using the `result` generator constructor from
 elm-random-extra and the `result` shrinker constrctor from elm-shrink.
 -}
 result : Investigator error -> Investigator value -> Investigator (Result error value)
-result errSpec valSpec =
-  investigator
-    (Random.Result.result errSpec.generator valSpec.generator)
-    (Shrink.result errSpec.shrinker valSpec.shrinker)
+result errorInvestigator successInvestigator =
+  merge (map Err errorInvestigator) (map Ok successInvestigator)
 
+
+--list : Investigator a -> Investigator (List a)
+--list investigator =
+
+
+
+
+{-}
+lazylist : Investigator a -> Investigator (LazyList a)
+lazylist investigator =
+  let
+      shrinkList l =
+        let
+            n =
+              Lazy.List.length l
+
+            shrinkOne l = lazy <| \() ->
+              case force l of
+                Lazy.List.Nil ->
+                  force Lazy.List.empty
+
+                Lazy.List.
+-}
+{-}
 {-| Investigator list constructor. Generates random lists of values of size
 between 0 and 10 from a given investigator generator using the `rangeLengthList`
 generator constructor from elm-random-extra and the `list` shrinker constructor
@@ -293,19 +558,6 @@ func5 invF =
     (Random.Function.func5 invF.generator)
     (Shrink.noShrink)
 
-{-| Shrink a value from an Investigator generator.
-
-    int : Investigator Int
-
-    shrink int 10 == [0,5,7,8,9]
--}
-shrink : Investigator a -> Shrinker a
-shrink = .shrinker
-
-{-| Extract a Random Generator from and an Investigator generator.
--}
-random : Investigator a -> Generator a
-random = .generator
 
 {-} Simple example
 
@@ -332,4 +584,5 @@ vector =
           `Random.andMap` random float
   in
       investigator generator shrinker
+-}
 -}
